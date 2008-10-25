a37400547ec6c9daff00dfa7a58b9b93425cdd7b
Randall E. Barker <simstorm@mac.com>
2008-10-24 17:00:03 -0700

    Mites wip.

--- scripts/mites.lua
+++ scripts/mites.lua
@@ -1,21 +1,97 @@
+local function validate_position (self, pos)
+   if pos:get_x () > self.arena.max:get_x () then
+      pos:set_x (self.arena.min:get_x () + (self.arena.max:get_x () - pos:get_x ()))
+   elseif pos:get_x () < self.arena.min:get_x () then
+      pos:set_x (self.arena.max:get_x () - (self.arena.min:get_x () - pos:get_x ()))
+   end
+   if pos:get_z () > self.arena.max:get_z () then
+      pos:set_z (self.arena.min:get_z () + (self.arena.max:get_z () - pos:get_z ()))
+   elseif pos:get_z () < self.arena.min:get_z () then
+      pos:set_z (self.arena.max:get_z () - (self.arena.min:get_z () - pos:get_z ()))
+   end
+end
+
+local function update_mites (self, time)
+   for i, m in ipairs (self.mites) do
+      local pos = dmz.object.position (m.object)
+      pos:set_z (pos:get_z () - (time * 1000))
+      validate_position (self, pos)
+      dmz.object.position (m.object, nil, pos)
+   end
+end
+
+local function update_mite_count (self, object, handle, count, oldCount)
+   self.log:error ("Mites:", count)
+   self.arena.min = dmz.object.position (object, "Minimum_Area")
+   self.arena.max = dmz.object.position (object, "Maximum_Area")
+   local mites = self.mites
+   local MinX = self.arena.min:get_x ()
+   local MaxX = self.arena.max:get_x () - MinX
+   local MinZ = self.arena.min:get_z ()
+   local MaxZ = self.arena.max:get_z () - MinZ
+   while #mites < count do
+      local m = {}
+      m.object = dmz.object.create ("mite")
+      dmz.object.position (m.object, nil, {
+         (MaxX * math.random ()) + MinX,
+         0,
+         (MaxZ * math.random ()) + MinZ,
+      })
+      dmz.object.activate (m.object)
+      dmz.object.set_temporary (m.object)
+      mites[#mites + 1] = m
+   end
+   while #mites > count do
+      mites[#mites] = nil
+   end
+end
+
+local function update_chip_count (self, object, handle, count, oldCount)
+   self.log:error ("Chips:", count)
+end
+
+local function update_area_minimum (self, object, handle, min)
+   self.log:error ("Setting arena min:", min)
+   self.arena.min = min
+end
+
+local function update_area_maximum (self, object, handle, max)
+   self.log:error ("Setting arena max:", max)
+   self.arena.max = max
+end
 
 local function start (self)
+   local callbacks = { update_object_counter = update_mite_count, }
+   self.objObs:register ("Mites", callbacks, self)
+   callbacks = { update_object_counter = update_chip_count, }
+   self.objObs:register ("Chips", callbacks, self)
+   callbacks = { update_object_position = update_area_minimum, }
+   self.objObs:register ("Minimum_Area", callbacks, self)
+   callbacks = { update_object_position = update_area_maximum, }
+   self.objObs:register ("Maximum_Area", callbacks, self)
+   self.tsHandle = self.timeSlice:create (update_mites, self, self.name)
+end
 
-   self.mite = dmz.object.create ("mite")
-   dmz.object.position (self.mite, nil, {0, 0, 0})
-   dmz.object.activate (self.mite)
-
+local function stop (self)
+   if self.tsHandle then self.timeSlice:destroy (self.tsHandle) self.tsHandle = nil end
 end
 
 function new (config, name)
-
    local self = {
       start_plugin = start,
+      stop_plugin = stop,
+      timeSlice = dmz.time_slice.new (),
       log = dmz.log.new ("lua." .. name),
+      objObs = dmz.object_observer.new (),
+      arena = {
+         min = dmz.vector.new (-3000, 0, -2000),
+         max = dmz.vector.new (3000, 0, 2000),
+      },
+      mites = {},
+      chips = {},
    }
 
    self.log:info ("Creating plugin: " .. name)
 
    return self
-
 end
