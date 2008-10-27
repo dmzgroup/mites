74f82837e9b5f20254147a8ad60f3412b41d5070
Randall E. Barker <simstorm@mac.com>
2008-10-27 11:24:55 -0700

    mites now have a heading

--- scripts/mites.lua
+++ scripts/mites.lua
@@ -1,3 +1,6 @@
+local Forward = dmz.math.forward ()
+local Up = dmz.math.up ()
+
 local function validate_position (self, pos)
    if pos:get_x () > self.arena.max:get_x () then
       pos:set_x (self.arena.min:get_x () + (self.arena.max:get_x () - pos:get_x ()))
@@ -14,7 +17,8 @@ end
 local function update_mites (self, time)
    for i, m in ipairs (self.mites) do
       local pos = dmz.object.position (m.object)
-      pos:set_z (pos:get_z () - (time * 1000))
+      local ori = dmz.object.orientation (m.object)
+      pos = pos + (ori:transform (Forward) * time * 1000)
       validate_position (self, pos)
       dmz.object.position (m.object, nil, pos)
    end
@@ -37,6 +41,7 @@ local function update_mite_count (self, object, handle, count, oldCount)
          0,
          (MaxZ * math.random ()) + MinZ,
       })
+      dmz.object.orientation (m.object, nil, dmz.matrix.new (Up, math.random () * dmz.math.TwoPi))
       dmz.object.activate (m.object)
       dmz.object.set_temporary (m.object)
       mites[#mites + 1] = m
