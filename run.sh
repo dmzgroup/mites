780ac550665f7c9ebef77bc392ff6584179f3ddc
Randall E. Barker <simstorm@mac.com>
2010-01-04 11:25:30 -0800

    Updated to use resource file.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 . ../scripts/envsetup.sh
 
-$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/arena.xml config/canvas.xml config/lua.xml config/input.xml config/version.xml $*
+$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/arena.xml config/canvas.xml config/lua.xml config/input.xml config/version.xml config/resource.xml $*
